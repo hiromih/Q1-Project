bb66e74e160853bcf388195cde875617090d1da7
Hiromi Heider <hiromiheider@gmail.com>
2016-12-07 12:17:48 -0800

    Close #10, Close #15; Set up display for each recipe result, Remove unnecessary card action

--- js/index.js
+++ js/index.js
@@ -19,9 +19,9 @@
     }
   };
 
-  const getParameters = function(formattedIngredients) {
+  const getParameters = function(ingredients) {
 
-    const parameters = formattedIngredients.map((ingredient) => {
+    const parameters = ingredients.map((ingredient) => {
       if(ingredient.indexOf(' ') !== -1) {
         return ingredient = ingredient.replace(/\s/g, '+');
       } else {
@@ -29,20 +29,22 @@
       }
     });
 
-    let allParams = `q=${parameters[0]}`;
+    return `q=${parameters.join('+')}`;
 
-    for (let i = 0; i < parameters.length; i++) {
-      allParams += `&allowedIngredient[]=${parameters[i]}`;
-    }
-
-    return allParams;
+    //might use this later if I can get better results.
+    // let allParams = `q=${parameters[0]}`;
+    //
+    // for (let i = 0; i < parameters.length; i++) {
+    //   allParams += `&allowedIngredient[]=${parameters[i]}`;
+    // }
+    // return allParams;
 
   };
 
   const getRecipes = function(ingredients) {
 
     const allParameters = getParameters(ingredients);
-
+    console.log(allParameters);
     const $xhr = $.ajax({
       method: 'GET',
       url: `http://api.yummly.com/v1/api/recipes?${allParameters}&requirePictures=true`,
@@ -57,7 +59,7 @@
       if($xhr.status !== 200) {
         return;
       }
-      console.log(data);
+
       for(const result of data.matches) {
 
         const recipe = {
@@ -66,6 +68,9 @@
           ingredients: result.ingredients
         };
 
+        const ingredientResults = matchIngredients(ingredients, recipe.ingredients);
+        Object.assign(recipe, ingredientResults);
+        console.log(recipe);
         getImageAndSource(recipe);
       }
     });
@@ -74,8 +79,7 @@
       console.error(err);
     });
 
-    console.log(recipes);
-  }
+  };
 
   const getImageAndSource = function(recipe) {
 
@@ -109,31 +113,19 @@
 
   const createCard = function(recipe) {
     let $outerDiv = $('<div>').addClass('col s12 m6');
-    let $cardDiv = $('<div>').addClass('card small sticky-action');
-    let $imageDiv = $('<div>').addClass('card-image waves-effect waves-block waves-light');
-    const $img = $('<img>').addClass('activator').attr('src', recipe.image);
+    let $cardDiv = $('<div>').addClass('card small');
+    let $imageDiv = $('<div>').addClass('card-image');
+    const $img = $('<img>').attr('src', recipe.image);
     let $cardContentDiv = $('<div>').addClass('card-content');
-    let $contentSpan = $('<span>').addClass('card-title activator grey-text text-darken-4').text(recipe.name);
+    let $contentSpan = $('<span>').addClass('card-title grey-text text-darken-4').text(recipe.name);
     const $contentIcon = $('<i>').addClass('meterial-icons right');
     let $contentP = $('<p>');
     const $contentA = $('<a>').attr({ href:recipe.recipeUrl, target:"_blank" }).text('Get Recipe');
     let $actionDiv = $('<div>').addClass('card-action');
     let $actionRow = $('<div>').addClass('row');
-    const $actionPercentDiv = $('<div>').addClass('col s4').text('%');
-    const $actionFound = $('<div>').addClass('col s4').text('found');
-    const $actionNotFound = $('<div>').addClass('col s4').text('not found');
-    let $revealDiv = $('<div>').addClass('card-reveal');
-    let $revealRow = $('<div>').addClass('row');
-    const $revealFoundDiv = $('<div>').addClass('col s5').text('found');
-    const $revealNotFoundDiv = $('<div>').addClass('col s5').text('not found');
-    let $revealClearDiv = $('<div>').addClass('col s2');
-    let $revealIconSpan = $('<span>').addClass('card-title grey-text text-darken-4');
-    const $revealClearIcon = $('<i>').addClass('material-icons right').text('close');
-
-    $revealIconSpan = $revealIconSpan.append($revealClearIcon);
-    $revealClearDiv = $revealClearDiv.append($revealIconSpan);
-
-    $revealRow = $revealRow.append($revealFoundDiv).append($revealNotFoundDiv).append($revealClearDiv);
+    const $actionPercentDiv = $('<div>').addClass('col s4').text(recipe.percentMatch);
+    const $actionFound = $('<div>').addClass('col s4').text(recipe.matched);
+    const $actionNotFound = $('<div>').addClass('col s4').text(recipe.unmatched);
 
     $actionRow = $actionRow.append($actionPercentDiv).append($actionFound).append($actionNotFound)
 
@@ -143,20 +135,39 @@
     $imageDiv = $imageDiv.append($img);
     $cardContentDiv = $cardContentDiv.append($contentSpan).append($contentP);
     $actionDiv = $actionDiv.append($actionRow);
-    $revealDiv = $revealDiv.append($revealRow);
 
-    $cardDiv = $cardDiv.append($imageDiv).append($cardContentDiv).append($actionDiv).append($revealDiv);
+    $cardDiv = $cardDiv.append($imageDiv).append($cardContentDiv).append($actionDiv);
 
     $outerDiv = $outerDiv.append($cardDiv);
 
     $('.card-insert-point').append($outerDiv);
 
-  }
+  };
+
+  const matchIngredients = function (userIngredients, recipeIngredients) {
+    let matchedIngredients = 0;
+    let unmatchedIngredients = 0;
+    for(const userIngredient of userIngredients) {
+      for(const recipeIngredient of recipeIngredients) {
+        if(recipeIngredient.includes(userIngredient)) {
+          matchedIngredients++;
+          break;
+        }
+      }
+    }
+
+    const totals = {
+      matched: matchedIngredients,
+      unmatched: recipeIngredients.length - matchedIngredients,
+      percentMatch: Math.round(((matchedIngredients / recipeIngredients.length) * 100)) + "%"
+    };
+
+    return totals;
+  };
 
   $('form').submit((event) => {
       event.preventDefault();
       addToList();
-      $('.card-insert-point').empty();
   });
 
   $('i.add').click((event) => {
@@ -176,6 +187,7 @@
   $('.submitBtn').click((event) => {
     recipes = [];
     addToList();
+    $('.card-insert-point').empty();
 
     if($('ul.list li').length) {
       const itemsToSearch = [];
@@ -188,11 +200,8 @@
 
       getRecipes(itemsToSearch);
 
-    } else if(!$('ul.list li').length) {
-        Materialize.toast('Please add ingredients.', 4000);
-
     } else {
-        Materialize.toast('Please select a search type.', 4000);
+        Materialize.toast('Please add ingredients.', 4000);
     }
   });
 
