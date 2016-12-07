c3fcaa93b80a824239e0e8d3f10769da938001b4
Hiromi Heider <hiromiheider@gmail.com>
2016-12-06 17:48:55 -0800

    Close #14 Clear old search results

--- js/index.js
+++ js/index.js
@@ -97,6 +97,8 @@
       recipe.image = data.images[0].hostedLargeUrl;
       recipe.recipeUrl = data.source.sourceRecipeUrl;
       recipes.push(recipe);
+
+      createCard(recipe);
     });
 
     $xhr.fail((err) => {
@@ -105,9 +107,56 @@
 
   };
 
+  const createCard = function(recipe) {
+    let $outerDiv = $('<div>').addClass('col s12 m6');
+    let $cardDiv = $('<div>').addClass('card small sticky-action');
+    let $imageDiv = $('<div>').addClass('card-image waves-effect waves-block waves-light');
+    const $img = $('<img>').addClass('activator').attr('src', recipe.image);
+    let $cardContentDiv = $('<div>').addClass('card-content');
+    let $contentSpan = $('<span>').addClass('card-title activator grey-text text-darken-4').text(recipe.name);
+    const $contentIcon = $('<i>').addClass('meterial-icons right');
+    let $contentP = $('<p>');
+    const $contentA = $('<a>').attr({ href:recipe.recipeUrl, target:"_blank" }).text('Get Recipe');
+    let $actionDiv = $('<div>').addClass('card-action');
+    let $actionRow = $('<div>').addClass('row');
+    const $actionPercentDiv = $('<div>').addClass('col s4').text('%');
+    const $actionFound = $('<div>').addClass('col s4').text('found');
+    const $actionNotFound = $('<div>').addClass('col s4').text('not found');
+    let $revealDiv = $('<div>').addClass('card-reveal');
+    let $revealRow = $('<div>').addClass('row');
+    const $revealFoundDiv = $('<div>').addClass('col s5').text('found');
+    const $revealNotFoundDiv = $('<div>').addClass('col s5').text('not found');
+    let $revealClearDiv = $('<div>').addClass('col s2');
+    let $revealIconSpan = $('<span>').addClass('card-title grey-text text-darken-4');
+    const $revealClearIcon = $('<i>').addClass('material-icons right').text('close');
+
+    $revealIconSpan = $revealIconSpan.append($revealClearIcon);
+    $revealClearDiv = $revealClearDiv.append($revealIconSpan);
+
+    $revealRow = $revealRow.append($revealFoundDiv).append($revealNotFoundDiv).append($revealClearDiv);
+
+    $actionRow = $actionRow.append($actionPercentDiv).append($actionFound).append($actionNotFound)
+
+    $contentP = $contentP.append($contentA);
+    $contentSpan = $contentSpan.append($contentIcon);
+
+    $imageDiv = $imageDiv.append($img);
+    $cardContentDiv = $cardContentDiv.append($contentSpan).append($contentP);
+    $actionDiv = $actionDiv.append($actionRow);
+    $revealDiv = $revealDiv.append($revealRow);
+
+    $cardDiv = $cardDiv.append($imageDiv).append($cardContentDiv).append($actionDiv).append($revealDiv);
+
+    $outerDiv = $outerDiv.append($cardDiv);
+
+    $('.card-insert-point').append($outerDiv);
+
+  }
+
   $('form').submit((event) => {
       event.preventDefault();
       addToList();
+      $('.card-insert-point').empty();
   });
 
   $('i.add').click((event) => {
@@ -121,6 +170,7 @@
   $('.clear-search').click((event) => {
     $('ul.list').empty();
     $('p.search-terms').empty();
+    $('.card-insert-point').empty();
   });
 
   $('.submitBtn').click((event) => {
